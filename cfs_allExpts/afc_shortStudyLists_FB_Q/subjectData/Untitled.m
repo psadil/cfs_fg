6877d2e7b789b72c9d60dc79b1bdb542d6601c44
psadil <psadil@gmail.com>
2016-04-09 21:59:50 -0400

    testing out new folder structure

--- cfs_allExpts/afc_shortStudyLists_FB_Q/subjectData/Untitled.m
+++
@@ -1,7 +0,0 @@
-rob = java.awt.Robot;
-
-rob.keyPress(java.awt.event.KeyEvent.VK_1);
-rob.keyRelease(java.awt.event.KeyEvent.VK_1);
-
-% peval([ 'rob.keyPress(java.awt.event.KeyEvent.VK_', upper(1), ');' ]);
-% eval([ 'rob.keyRelease(java.awt.event.KeyEvent.VK_', upper(1), ');' ]);
--- cfs_allExpts/afc_shortStudyLists_FB_Q/subjectData/itemCondition.m
+++
@@ -1,7 +0,0 @@
-conditionOrder_study = p.itemCondition_study(p.studyOrder);
-
-conditionOrder_test=zeros(1,p.nItems);
-for testTrial = 1:p.nItems
-    item = p.testOrder(testTrial);
-    conditionOrder_test(testTrial) = conditionOrder_study(find(p.studyOrder==item));
-end
